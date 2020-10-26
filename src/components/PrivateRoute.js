e43617d7505ed45e56afe0593706fc6673832f99
Felipe Menezes <felipe.menezes@ahgora.com.br>
2020-10-25 21:13:21 -0300

    feat: private route component

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,25 @@
+import { Redirect, Route } from 'react-router-dom';
+import { SESSION_PROPERTY, ROUTES } from '../constants';
+
+const storage = window.localStorage;
+
+const isUserAuthenticated = () => {
+  const token = storage.getItem(SESSION_PROPERTY.TOKEN);
+  const email = storage.getItem(SESSION_PROPERTY.EMAIL);
+
+  if (!token || !email) return false;
+  
+  return true;
+};
+
+const PrivateRoute = ({ ...props }) => {
+  const isAuth = isUserAuthenticated();
+
+  if (!isAuth) {
+    return <Redirect to={ROUTES.LOGIN} />;
+  }
+
+  return <Route {...props} />;
+};
+
+export default PrivateRoute;
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  DEFAULT: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  DASHBOARD: '/dashboard',
+};
+
+export const SESSION_PROPERTY = {
+  TOKEN: 'USER_TOKEN',
+  EMAIL: 'USER_EMAIL',
+};
