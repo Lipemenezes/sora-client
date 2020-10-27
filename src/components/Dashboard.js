af1e2a04e839d140603e4e85a453832dfccbb8b5
Felipe Menezes <felipe.menezes@ahgora.com.br>
2020-10-27 00:27:34 -0300

    feat: basic header v1

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
+import { Container, Box } from '@material-ui/core/';
+
+import Header from './Header';
 
 export default function Dashboard() {
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="100%" disableGutters>
+      <Box>
+        <Header></Header>
+      </Box> 
       Welcome to your dashboard =]
     </Container>
   );
